e9bf1563dcc4f7f5759b80a5e8f77000cd3267e0
Paul McQuade <paulmcquad@gmail.com>
2019-11-08 18:35:23 +0000

    Moved menu to menu.php
    Used in index and resources
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ menu.php
@@ -0,0 +1,25 @@
+<?php
+echo '
+<nav class="navbar navbar-default container" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        
+    </div>
+    <div class="collapse navbar-collapse navbar-responsive-collapse">
+        <ul class="nav navbar-nav sm" data-smartmenus-id="15724499035408755">
+            
+            <li><a href="https://coder-forge.com/">Home</a></li>
+            <li class="dropdown"><a href="https://coder-forge.com/meetups/">Meetups</a></li>
+            <li class="dropdown"><a href="https://coder-forge.com/resources.html">Resources</a></li>
+            <li class="dropdown"><a href="https://coder-forge.com/forgers">Forgers</a></li>
+            <li class="dropdown"><a href="https://coder-forge.com/about/">About</a></li>
+        </ul>
+    </div>
+</nav>
+';
+?>
