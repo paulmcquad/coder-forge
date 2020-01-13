08aaf52b8f77b8fdb9bf575d4ed868a44f34456a
Paul McQuade <paulmcquad@gmail.com>
2020-01-13 18:17:57 +0000

    Remove Meetup and About links
    No pages for them
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- menu.php
+++ menu.php
@@ -14,10 +14,10 @@ echo '
         <ul class="nav navbar-nav sm" data-smartmenus-id="15724499035408755">
             
             <li><a href="https://coder-forge.com/index.php">Home</a></li>
-            <li class="dropdown"><a href="https://coder-forge.com/meetups.php">Meetups</a></li>
+            <!--  <li class="dropdown"><a href="https://coder-forge.com/meetups.php">Meetups</a></li> -->
             <li class="dropdown"><a href="https://coder-forge.com/resources.php">Resources</a></li>
             <li class="dropdown"><a href="https://coder-forge.com/forgers.php">Forgers</a></li>
-            <li class="dropdown"><a href="https://coder-forge.com/about.php">About</a></li>
+            <!-- <li class="dropdown"><a href="https://coder-forge.com/about.php">About</a></li> -->
         </ul>
     </div>
 </nav>
