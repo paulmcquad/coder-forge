95815f2a74e525c93a23912b01ac28878947e986
Paul McQuade <paulmcquad@gmail.com>
2020-06-16 19:58:48 +0100

    Updated WhatsApp Information
    Removed Where Do We Meet?
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- index.php
+++ index.php
@@ -54,7 +54,7 @@
                     <p>Due to the Covid-19 situation, we are unable to meet in our usual venue in Temple Bar for the moment.
                      However, we are continuing the meetings every Tuesday 7pm - 9pm on Zoom.
                      Anyone is welcome to join the meeting who want to learn some coding.
-                     To join, you need to become a member of our WhatsApp Group(see below).
+                     To join, you need to become a member of our <a href="https://web.whatsapp.com/" color="green">WhatsApp</a>.
 
                      At the moment, we are doing API usage based on the Twilio app, where we are using Postman to manage the web APIs.
                      It's a fun API where you can learn how to send text messages from your terminal or website to anyone using the Twilio API.
@@ -74,18 +74,18 @@
                             </div>
                         </section>
 
-                    <h3>Where Do We Meet?</h3>
+                    <!-- <h3>Where Do We Meet?</h3> -->
 
-                    <section class="block-paragraph">
-                        <div class="rich-text">
-                        <p>
-                            We meet every Tuesdays from (19h to 21h) or (7pm - 9pm) at the Brick Alley Cafe, 25 Essex St E, Temple Bar, Dublin, D02 W560! 
-                        </p>
-                        <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.880054839293!2d-6.266677584269861!3d53.34540327997877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670fe44ae2b6e9%3A0xd679b49e440fc45d!2sBrick+Alley+Cafe!5e0!3m2!1sen!2sie!4v1565120761707!5m2!1sen!2sie"
-                        width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-                        </div>
-                    </section>
+                    <!-- <section class="block-paragraph"> -->
+                        <!-- <div class="rich-text"> -->
+                        <!-- <p> -->
+                            <!-- We meet every Tuesdays from (19h to 21h) or (7pm - 9pm) at the Brick Alley Cafe, 25 Essex St E, Temple Bar, Dublin, D02 W560!  -->
+                        <!-- </p> -->
+                        <!-- <iframe -->
+                        <!-- src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2381.880054839293!2d-6.266677584269861!3d53.34540327997877!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48670fe44ae2b6e9%3A0xd679b49e440fc45d!2sBrick+Alley+Cafe!5e0!3m2!1sen!2sie!4v1565120761707!5m2!1sen!2sie" -->
+                        <!-- width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+                        <!-- </div> -->
+                    <!-- </section> -->
 
                     <h3>How Is It Done?</h3>
 
