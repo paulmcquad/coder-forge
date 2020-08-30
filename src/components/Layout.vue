e895f1fcafd104d94c8ba0e27cc374808a5593d6
Paul McQuade <paulmcquad@gmail.com>
2020-08-30 19:39:00 +0100

    Started with Baseline Layout
    v-navigation-drawer used
    
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ src/components/Layout.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="#faac33" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Coder Forge</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--       <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn :href="source" icon large target="_blank" v-on="on">
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>-->
+    </v-main>
+    <v-footer color="#faac33" app>
+      <span class="white--text">
+        Paul McQuade
+        <span class="copyleft">&copy;</span>
+        {{ new Date().getFullYear() }} -
+        GNU General Public License v3 -
+        <a
+          href="https://github.com/paulmcquad/coder-forge"
+          class="white--text"
+        >https://github.com/paulmcquad/coder-forge</a>
+      </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+  },
+
+  data: () => ({
+    drawer: null,
+  }),
+};
+</script>
+
+<style>
+.copyleft {
+  display: inline-block;
+  transform: rotate(180deg);
+}
+</style>
