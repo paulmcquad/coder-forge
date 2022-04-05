866f1c0d26afd2fc7121309129501cb8cc492325
Paul McQuade <paulmcquad@gmail.com>
2022-04-05 19:01:35 +0100

    Added React Router v6
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,10 @@ import '../App.css';
 
 export default function Navbar() {
 return(
+<header>
 <nav class="px-2 bg-white border-gray-200 dark:bg-gray-800 dark:border-gray-700">
   <div class="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="#" class="flex items-center">
+    <a href="https://coderforge.io/" class="flex items-center">
         <img src="https://coderforge.io/assets/logo.png" class="mr-3 h-6 sm:h-10" alt="CoderForge Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">CoderForge</span>
     </a>
@@ -44,10 +45,13 @@ return(
           <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Pricing</a>
         </li>
         <li>
-          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Contact</a>
+          <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 dark:hover:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">About</a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+
+</header>
+
 )}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'flowbite';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Navbar />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
@@ -17,3 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
