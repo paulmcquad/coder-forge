3014dcd293909ed8453d37d421d148585c9bcafd
Paul McQuade <paulmcquad@gmail.com>
2022-04-06 23:07:03 +0100

    Remove formatting
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Link } from "react-router-dom"; // reactrouter v6
+// import { Link } from "react-router-dom"; // reactrouter v6
 
 const About = () => {
 return (
 	<div
-	style={{
+/* 	style={{
 		display: 'flex',
 		justifyContent: 'Right',
 		alignItems: 'Right',
 		height: '100vh'
-	}}
+	}} */
 	>
 	<h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
-        <Link to="/">Home</Link> |
-        <Link to="/about">About</Link> |
+        {/* <Link to="/">Home</Link> |
+        <Link to="/about">About</Link> | */}
 
 
 	</div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Link } from "react-router-dom"; // reactrouter v6
+// import { Link } from "react-router-dom"; // reactrouter v6
 
 const Home = () => {
 return (
 	<div
-	style={{
+/* 	style={{
 		display: 'flex',
 		justifyContent: 'Right',
 		alignItems: 'Right',
 		height: '100vh'
-	}}
+	}} */
 	>
     <h2>Welcome to the homepage!</h2>
     <p>You can do this, I believe in you.</p>
 
-    <Link to="/">Home</Link> |
-    <Link to="/about">About</Link> |
+    {/* <Link to="/">Home</Link> |
+    <Link to="/about">About</Link> | */}
 
 	</div>
 );
